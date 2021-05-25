7baa31e09d73745b88c249efc74e388b87db801a
mbish08 <mdrew8@gmail.com>
2021-05-24 17:13:53 -0700

    commit tree 5442305f2f094d176f621c63ef84b38df8f14314

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,9 +4,39 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map((director, i) => (
+        <div key={i}>
+          <h3>Name: {director.name}</h3>
+          <p>Movies:</p>
+          <ul>
+            {director.movies.map((movie, i) => (
+              <li key={i}>{movie}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 }
 
 export default Directors
+
+
+// return (
+//   <div>
+//     <h1>Actors Page</h1>
+//     {actors.map((actor, index) => (
+//         <div key={index}>
+//             <h3>Name: {actor.name}</h3>
+//             <p>Movies:</p>
+//             <ul>
+//                 {actor.movies.map((movie, index) => (
+//                     <li key={index}>{movie}</li>
+//                 ))}
+//             </ul>
+//         </div>
+//     ))}
+//   </div>
+// );
+// };
